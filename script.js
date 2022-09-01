5e6b23af17e4ad6caa516df4d83a5586e84268f1
Zek <nzekic@hotmail.com>
2022-09-01 18:02:42 +0200

    basics

---
+++ script.js
@@ -0,0 +1,24 @@
+// window.onscroll = function() {
+//     scrollFunction()
+// };
+
+// function scrollFunction() {
+//     if (document.documentElement.scrollTop > 0) {
+//         document.getElementById("header").style.backgroundColor = "#222831";
+//     } else {
+//         document.getElementById("header").style.opacity = "0.1";
+//     }
+// }
+
+// $(function() {
+//     var header = $(".fade-transparent");
+//     $(window).scroll(function() {
+//         var scroll = $(window).scrollTop();
+
+//         if (scroll >= 200) {
+//             header.removeClass('fade-transparent').addClass("fade-background");
+//         } else {
+//             header.removeClass("fade-background").addClass('fade-transparent');
+//         }
+//     });
+// });
