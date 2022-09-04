eac987268c55f7135263726ff3eb513f2b33397b
Zek <nzekic@hotmail.com>
2022-09-04 18:54:14 +0200

    form works

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 
-const form = document.getElementById('right-form');
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbygsw3mxUM8TVr4nYGdjcCgOod9Pvu_Ze1if4UJko-SdNITpwluvsL7BmUKVZjFMpg5/exec'
+  const form = document.forms['right-form']
 
-form.addEventListener('submit', ()  => {
-
-
-    // form.reset();
-});
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => console.log('Success!', response))
+      .catch(error => console.error('Error!', error.message))
+      form.reset();
+  })
