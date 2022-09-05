892109664e417422251af7407040edbf25931bc1
Zek <91539805+ZekNe@users.noreply.github.com>
2022-09-05 22:13:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-
   const scriptURL = 'https://script.google.com/macros/s/AKfycbygsw3mxUM8TVr4nYGdjcCgOod9Pvu_Ze1if4UJko-SdNITpwluvsL7BmUKVZjFMpg5/exec'
   const form = document.forms['right-form']
 
+  
   form.addEventListener('submit', e => {
     e.preventDefault()
     fetch(scriptURL, { method: 'POST', body: new FormData(form)})
       .then(response => console.log('Success!', response))
       .catch(error => console.error('Error!', error.message))
       form.reset();
+    
   })
